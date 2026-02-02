6bc788d9cd57ad868333cf3c4c2bd77de2047282
Michał Jankowski <jankowski.michal1@outlook.com>
2026-02-02 20:50:24 +0100

    Naprawa konfliktu pluginow, aktywacja Room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
 }
 
 android {
     namespace = "com.example.mytodoapp"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.mytodoapp"
@@ -42,4 +42,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
 }
---
+++ app/src/main/java/com/example/mytodoapp/TaskDao.kt
@@ -0,0 +1,11 @@
+package com.example.mytodoapp
+import androidx.room.*
+
+@Dao
+interface TaskDao {
+    @Insert
+    fun insert(task: Task)
+
+    @Query("SELECT * FROM tasks")
+    fun getAll(): List<Task>
+}
